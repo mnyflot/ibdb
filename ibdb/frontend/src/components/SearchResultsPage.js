3f5c0089727c41cc472c7b0c09e48437a3fd43db
Rami Chumber <ramic@stud.ntnu.no>
2023-02-23 11:56:10 +0100

    La til fetch for å hente ut boken man har søkt på

--- ibdb/frontend/src/components/SearchResultsPage.js
+++ ibdb/frontend/src/components/SearchResultsPage.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function SearchResultsPage() {
+    const navigate = useNavigate();
     let { searchPhrase } = useParams();
     searchPhrase = searchPhrase.split("+");
     searchPhrase = searchPhrase.join(" ");
-    searchPhrase = searchPhrase.toLowerCase();
+    // searchPhrase = searchPhrase.toLowerCase();
+
 
     useEffect(() => {
-        // Fetch book data based on the bookId parameter
+        // Fetch book data based on the searchPhrase parameter
         fetch(`/get-book?title=${searchPhrase}`)
           .then(response => response.json())
-          .then(data => setBook(data))
+          .then(data => navigate(`/book/${data.bookId}`))
           .catch(error => console.error(error));
       }, [searchPhrase]);
 
