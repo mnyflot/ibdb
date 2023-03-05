987dd9285e73761da0b96c3529f12d8d236cbf41
Victoria <vmeilert@stud.ntnu.no>
2023-03-05 20:38:23 +0100

    Issue #42 er ferdigstilt. Henter ut bøker fra databasen, og sendes til riktig side når man trykker på bok.  Coworker: @dabjorge

--- ibdb/frontend/src/components/Book.js
+++ ibdb/frontend/src/components/Book.js
@@ -15,30 +15,37 @@ export default function Book() {
   });
 
   useEffect(() => {
-    // Fetch book data based on the bookId parameter
     fetch(`/get-book?bookId=${bookId}`)
       .then(response => response.json())
       .then(data => setBook(data))
       .catch(error => console.error(error));
   }, [bookId]);
 
+    const overalRating = Math.round(book.totalRatingScore/book.numberOfRatings * 10) / 10
+    var stars = [];
+
+    for (let index = 0; index < 5; index++) {
+        index < Math.round(overalRating) ? stars.push(<div>&#9733;</div>) : stars.push(<div>&#9734;</div>);
+    }
+
   return (
     <div>
         <Header/>
         <div className='bookMain'>
             <div className='bookImage'>
-                <img src="https://dj4fsg3e1je59.cloudfront.net/9788202593209/img/0"/>
+                <img className='bookCover' src={book.imageURL}/>
             </div>
             <div className='bookInformation'>
                 <h1>{book.title} </h1>
-                <p id='authorAndYear'>Erlend Loe {book.author} &#8226; 2018 {book.year} &#8226; Roman {book.genre}</p>
-                <p> &#9733; &#9733;  &#9733;  &#9733; &#9734; <b>4.3</b>/5 </p>
+                <p id='authorAndYear'>{book.author} &#8226; {book.year} &#8226; {book.genre}</p>
+                <p> 
+                <div className='stars'>
+                    {stars}
+                </div>
+                 <b>{overalRating}</b>/5 </p>
                 <hr></hr>
                 <p> <b>OM BOKEN</b></p>
-                <p> {book.description} JEG GIR STORT SETT FAEN I ROM, MEN JEG HAR PROBLEMER MED TID. Den 25 år gamle hovedpersonen har to venner. En god og en dårlig. Og så har han en bror som ikke er så altfor sympatisk. 
-                    Når denne broren slår ham i krokket, raser tilværelsen hans sammen. Han mister sin begeistring og troen på at alt vil gå bra til slutt og havner i en eksistensiell krise. Han bryter 
-                    opp fra sitt vanlige liv, dropper studiene og låner brorens leilighet. Der tilbringer han tiden dels med å leke dels med å reflektere over ting han liker og misliker. Det hele er 
-                    skildret med en særlig blanding av ironi og naivitet der en lengsel etter den tapte barndom ligger under.</p>
+                <p> {book.description}</p>
             </div>
         </div>
     </div>
--- ibdb/frontend/src/components/HomePage.js
+++ ibdb/frontend/src/components/HomePage.js
@@ -17,9 +17,9 @@ export class HomePage extends Component {
             </div>
             <div>
             </div>
-            <BookButton/>
+                <BookButton/>
           </div>
-        );
+        );  
         
     } 
 
