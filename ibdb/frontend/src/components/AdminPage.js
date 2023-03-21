b0ea30eb3ddf292a5ac0f58f464a549ead105820
Victoria <vmeilert@stud.ntnu.no>
2023-03-21 17:25:24 +0100

    Lagt til tilbake-knapp på bok. Og ellers små CSS endringer.

--- ibdb/frontend/src/components/AdminPage.js
+++ ibdb/frontend/src/components/AdminPage.js
@@ -6,12 +6,17 @@ export default function AdminPage() {
 
     return(
         <div>
-            <div>
-                <NewBookPage></NewBookPage>
+            <h3 className="categoryFrontpage">Administrator</h3>
+            <hr className="underlineFrontpage"></hr>
+            <div className="adminFeatures">
+                <div>
+                    <NewBookPage></NewBookPage>
+                </div>
+                <div >
+                    <CreateAdminUser></CreateAdminUser>
+                </div>
             </div>
-            <div >
-                <CreateAdminUser></CreateAdminUser>
-            </div>
+           
             <div>
                 <hr className="underlineMakeNewBook"></hr>
             </div>
